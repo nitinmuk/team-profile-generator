8c30c32d43e29c569ab04a1ad57306d5bbe579e7
Nitin Mukesh <nitinmukes@gmail.com>
2020-07-18 00:59:05 +0800

    Added questions and logic to accept employee details and write to a html file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const questions = require("./lib/questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,26 +11,46 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+console.log("Welcome to Team Profile Generator. Let us build an engineering team");
+acceptEmployees();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+/**
+ * accepts employees details recursively
+ */
+function acceptEmployees() {
+    inquirer.prompt(questions).then((answers) => {
+        let emp;
+        switch (answers.role) {
+            case 'Manager':
+                emp = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                break;
+            case 'Engineer':
+                emp = new Engineer(answers.name, answers.id, answers.email, answers.githubUser);
+                break;
+            case 'Intern':
+                emp = new Intern(answers.name, answers.id, answers.email, answers.schoolName);
+                break;
+        }
+        employees.push(emp);
+        if (answers.continue) {
+            acceptEmployees();
+        }
+        else {
+            console.log("Thanks for providing all details. Generating Team profile in output folder..");
+            writeToFile(render(employees));
+        }
+    });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const writeToFile = (html) => {
+    // check if output folder exists, if not create same
+    if(!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdir(OUTPUT_DIR, error => error ? console.log(error) : console.log("Output folder created..."));      
+    }
+    fs.writeFile(outputPath, html, error =>
+        error ? console.log(error) : console.log("Team profile generated successfully in output folder.")
+    );
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+}
